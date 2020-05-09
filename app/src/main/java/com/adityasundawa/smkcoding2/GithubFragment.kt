1c380b0ca2b4a2f141a03937968dcfa86b930926
hendra0ma <adityasundawa.co@gmail.com>
2020-05-09 23:58:42 +0700

    Tambah Fragment +SELESAI BAB 1+

---
+++ app/src/main/java/com/adityasundawa/smkcoding2/GithubFragment.kt
@@ -0,0 +1,27 @@
+package com.adityasundawa.smkcoding2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+
+class GithubFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_github,
+            container, false)
+    }
+    override fun onViewCreated(
+        view: View,
+        @Nullable savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/adityasundawa/smkcoding2/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.adityasundawa.smkcoding2
+
+import MyFriendsFragment
+import android.content.Context
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+FragmentStateAdapter(fragmentActivity) {
+    private val JUMLAH_MENU = 3
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> { return MyFriendsFragment() }
+            1 -> { return GithubFragment() }
+            2 -> { return ProfilFragment() }
+            else -> {
+                return GithubFragment()
+            }
+        }
+    }
+    override fun getItemCount(): Int {
+        return JUMLAH_MENU
+    }
+}
